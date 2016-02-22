e76f7567745cacb6c4f432373415f3fad04836b4
hehe876 <ndao3@asu.edu>
2016-02-22 10:02:38 -0700

    Initial Version

---
+++ cse360assign3/src/cse360assign3/CalculatorTest.java
@@ -0,0 +1,40 @@
+package cse360assign3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void testCalculator() {
+		Calculator dummy = new Calculator();
+		assertNotNull(dummy);
+	}
+	
+	public void testgetTotal(){
+		fail("Not yet implemented");
+	}
+	
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+	
+	public void testSubtract() {
+		fail("Not yet implemented");
+	}
+	
+	public void testMultiply() {
+		fail("Not yet implemented");
+	}
+	
+	public void testDivide() {
+		fail("Not yet implemented");
+	}
+	
+	public void testGetHistory() {
+		fail("Not yet implemented");
+	}
+	
+
+}
